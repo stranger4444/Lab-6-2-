1dac33207299c5d05a8ebcc2ba3aa2d87aa75d6b
Denys Dronov <dronovdenis36@gmail.com>
2023-11-09 14:16:46 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp14/MathOperations.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class MathOperations
+    {
+        public static T Add<T>(T a, T b)
+        {
+            dynamic dynamicA = a;
+            dynamic dynamicB = b;
+            return dynamicA + dynamicB;
+        }
+
+        public static T Subtract<T>(T a, T b)
+        {
+            dynamic dynamicA = a;
+            dynamic dynamicB = b;
+            return dynamicA - dynamicB;
+        }
+
+        public static T Multiply<T>(T a, T b)
+        {
+            dynamic dynamicA = a;
+            dynamic dynamicB = b;
+            return dynamicA * dynamicB;
+        }
+
+        
+
+        public static void PrintArray<T>(T[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintMatrix<T>(T[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintTensor<T>(T[,,] tensor)
+        {
+            int dimensions = tensor.Rank;
+
+            if (dimensions < 3)
+            {
+                Console.WriteLine("Invalid tensor dimensions.");
+                return;
+            }
+
+            int depth = tensor.GetLength(0);
+            int rows = tensor.GetLength(1);
+            int columns = tensor.GetLength(2);
+
+            for (int i = 0; i < depth; i++)
+            {
+                Console.WriteLine($"Tensor Layer {i + 1}:");
+                for (int j = 0; j < rows; j++)
+                {
+                    for (int k = 0; k < columns; k++)
+                    {
+                        Console.Write($"{tensor[i, j, k]} ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleApp14;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int a = 5, b = 3;
+        Console.WriteLine($"Addition: {MathOperations.Add(a, b)}");
+        Console.WriteLine($"Subtraction: {MathOperations.Subtract(a, b)}");
+        Console.WriteLine($"Multiplication: {MathOperations.Multiply(a, b)}");
+
+        double[] arrayA = { 1.5, 2.5, 3.5 };
+        double[] arrayB = { 0.5, 1.5, 2.5 };
+        Console.Write("Array Addition: ");
+        MathOperations.PrintArray(MathOperations.Add(arrayA, arrayB));
+
+        int[,] matrixA = { { 1, 2 }, { 3, 4 } };
+        int[,] matrixB = { { 5, 6 }, { 7, 8 } };
+        Console.WriteLine("Matrix Addition:");
+        MathOperations.PrintMatrix(MathOperations.Add(matrixA, matrixB));
+
+        float[,,] tensorA = { { { 1.0f, 2.0f }, { 3.0f, 4.0f } }, { { 5.0f, 6.0f }, { 7.0f, 8.0f } } };
+        float[,,] tensorB = { { { 0.1f, 0.2f }, { 0.3f, 0.4f } }, { { 0.5f, 0.6f }, { 0.7f, 0.8f } } };
+        Console.WriteLine("Tensor Addition:");
+        MathOperations.PrintTensor(MathOperations.Add(tensorA, tensorB));
+    }
+}
